8aa6eb51de1de1aa7544734ccdd31d4b24ade014
Diego <diehernandez@alumnos.uai.cl>
2018-10-02 15:12:27 -0300

    Updates

---
+++ precache-manifest.b0434a3b17d53906f27b06d2cf211807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "04cd5fdad8b6ec6db140",
+    "url": "/react-gh-pages/static/js/main.04cd5fda.chunk.js"
+  },
+  {
+    "revision": "c3e88a00f4566c1df320",
+    "url": "/react-gh-pages/static/js/1.c3e88a00.chunk.js"
+  },
+  {
+    "revision": "04cd5fdad8b6ec6db140",
+    "url": "/react-gh-pages/static/css/main.4385f573.chunk.css"
+  },
+  {
+    "revision": "32f4525cb4dc554d761c5519f1dad2a3",
+    "url": "/react-gh-pages/index.html"
+  }
+];
